d729d78e3e1f4e123575c566c251a34d0ccbb3e5
immathiias <dev.mathiasx@gmail.com>
2026-02-11 23:49:31 -0300

    resolve conflito

--- app/api/fornecedor/primeiro-acesso/route.ts
+++ app/api/fornecedor/primeiro-acesso/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // use service role aqui
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY! // use service role aqui
 );
 
 export async function POST(req: Request) {
